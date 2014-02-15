2867234660d2060154156919a9af1f40fb8afdc2
Will Gester <wiilgester@gmail.com>
2014-02-15 11:37:47 -0800

    Corrected address for db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var users = require("./app/models/user");
-var db  = mongoose.connect('ds033569.mongolab.com:33569/heroku_app22242686');
+var db  = mongoose.connect('mongodb://heroku_app22242686:do2856rbauaa0nlb1epfqgj61o@ds033569.mongolab.com:33569/heroku_app22242686');
 var app = express();
 
 require('./config/express')(app);
