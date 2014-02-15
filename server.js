12beb960d97da6f7d880e0257a43c351a8640833
Arkady Pevzner <arkady.pevzner@gmail.com>
2014-02-15 13:49:15 -0800

    Bootstapped front end for development

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require("express");
 var mongoose = require("mongoose");
 var users = require("./app/models/user");
 var db  = mongoose.connect('mongodb://localhost/copia');
+var consolidate = require('consolidate');
 var app = express();
 
 require('./config/express')(app);
+
+app.engine('html', consolidate.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/app/views');
 
 var admins =  require('./app/routes/users')(app);
 
