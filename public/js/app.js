bff385883268c21f2d50c6073c99b056fe9b45cd
Arkady Pevzner <arkady.pevzner@gmail.com>
2014-02-15 15:59:32 -0800

    Implements first pass at rendering home page; deletes unnecessary files in boilerplate;

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
-angular.module('app', []);
+var app = angular.module('app', [
+  'ngRoute'
+]);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/index.html',
+        controller: 'IndexController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }
+]);
