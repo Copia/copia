cad9f737daee4a90e86ec486a1593c3cd1eee358
arkadyp <arkady.pevzner@gmail.com>
2014-02-18 10:48:30 -0800

    Adds ng-model to relevant fields on borrow view/controller

--- public/js/controllers/borrow.js
+++ public/js/controllers/borrow.js
@@ -1,4 +1,9 @@
 angular.module('app')
 .controller('BorrowController', ["$scope", function($scope){
-
+  $scope.loanAmount = undefined;
+  $scope.paybackAmount = undefined; 
+  $scope.debtDueDate = undefined;
+  $scope.debtDeadlineDate = undefined;
+  $scope.debtCategory = undefined;
+  $scope.debtReason = undefined;
 }]);
