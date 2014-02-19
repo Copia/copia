e1eae158ced47adb5db3f70c711cf80a0b1f0369
arkadyp <arkady.pevzner@gmail.com>
2014-02-18 16:29:34 -0800

    Adds custom validation to ensure that payback amount is greater than loan amount

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,20 @@
 'use strict';
+
+app.directive('paybackValidation', function(){
+  return {
+    require: 'ngModel',
+    link : function($scope, elm, attrs, ngModel) {
+
+      $scope.$watch(function(watched){
+        var loanAmt = watched.loanAttrs.loanAmount;
+        var paybackAmt = watched.loanAttrs.paybackAmount;
+
+        if(loanAmt <= paybackAmt){
+          ngModel.$setValidity('paybackThreshold', true);
+        } else {
+          ngModel.$setValidity('paybackThreshold', false);
+        }
+      });
+    }
+  }
+});
