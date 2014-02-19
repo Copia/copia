7df887f9a8e8c8db9db6e40f77e50e6102f1ceef
Will Gester <wiilgester@gmail.com>
2014-02-18 18:31:44 -0800

    caasjj - moves app configuration to package.json from config/express.js

--- config/express.js
+++ config/express.js
@@ -1,13 +1,13 @@
-'use strict';
-var express = require("express");
-var path = require('path');
-var rootPath = path.normalize(__dirname + '/../..');
+// 'use strict';
+// var express = require("express");
+// var path = require('path');
+// var rootPath = path.normalize(__dirname + '/../..');
 
-module.exports = function(app) {
+// module.exports = function(app) {
 
-  // TODO: DO NOT USE THE bodyParser IN PRODUCTION!
-  app.use( express.bodyParser() );
+//   // TODO: DO NOT USE THE bodyParser IN PRODUCTION!
+//   app.use( express.bodyParser() );
 
-  app.use( app.router );
-  app.use( express.static( rootPath + '/copia/public') );
-};
+//   app.use( app.router );
+//   app.use( express.static( rootPath + '/copia/public') );
+// };
