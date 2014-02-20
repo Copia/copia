6bce04e4cf5fa2c260d4bdfc5611dff18303738c
Will Gester <willgester@gmail.com>
2014-02-20 14:18:39 -0800

    Added post route for signup

--- public/js/controllers/signup.js
+++ public/js/controllers/signup.js
@@ -1,10 +1,25 @@
 angular.module('app')
-.controller('SignUpController', ["$scope", '$location',
+.controller('SignUpController', ["$scope", '$location', '$http',
   
-  function($scope, $location){
+  function($scope, $location, $http){
     
     $scope.signUp = function(){
-      console.log($scope.email, $scope.password);
+      var data = {
+        access_token: null,
+        balance: null,
+        expires_in: null,
+        user: null,
+        username: $scope.email,
+        organization: null,
+        password_hash: $scope.password,
+        password_salt: null,
+        session_token: null,
+        karma: 0,
+        refresh_token: null
+      };
+      $http.post('/signup', data).success(function(){
+        console.log($scope.email, $scope.password)  
+      });
     };
 
     $scope.splashPage = function(){
