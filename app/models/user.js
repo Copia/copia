7bc70ec96a94f11d979ec559a05f1007052bcfd3
caasjj <caasjj@gmail.com>
2014-02-19 16:41:15 -0800

    implements user schema that incorporates Venmo account

--- app/models/user.js
+++ app/models/user.js
@@ -1,51 +1,48 @@
 'use strict';
 
 /**
- * Module dependencies.
- */
+* Module dependencies.
+*/
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    crypto = require('crypto');
+   Schema = mongoose.Schema,
+   crypto = require('crypto');
 
 /**
- * User Schema
- */
+* User Schema
+*/
 var UserSchema = new Schema({
-    access_token: String,
-    balance: String,
-    expires_in: Number,
-    user: {
-        username: String,
-        first_name: String,
-        last_name: String,
-        display_name: String,
-        is_friend: Boolean,
-        friends_count: Number,
-        about: String,
-        email: {type: String, unique: true},
-        phone: String,
-        profile_picture_url: String,
-        id: String,
-        date_joined: String,
-    },
-    refresh_token: String
-    // display_name: String,
-    // first_name: String,
-    // email: {
-    //     type: String,
-    //     unique: true
-    // },
-    // password_hash: String,
-    // password_salt: String,
-    // session_token: String,
-    // karma: Number
-}, {
-    collection: "people"
+  access_token: String,
+  balance: String,
+  expires_in: Number,
+  user: {
+    username: String,
+    first_name: String,
+    last_name: String,
+    display_name: String,
+    is_friend: Boolean,
+    friends_count: Number,
+    about: String,
+    email: {type: String, unique: true},
+    phone: String,
+    profile_picture_url: String,
+    id: String,
+    date_joined: String,
+  },
+  refresh_token: String,
+  collection: "people",
+  username: {
+    type: String,
+    unique: true
+  },
+  password_hash: String,
+  password_salt: String,
+  session_token: String,
+  karma: Number
 });
 
 /**
- * Virtuals
- */
+* Virtuals
+*/
 // UserSchema.virtual('password').set(function(password) {
 //     this._password = password;
 //     this.salt = this.makeSalt();
@@ -55,17 +52,16 @@ var UserSchema = new Schema({
 // });
 
 /**
- * Validations
- */
+* Validations
+*/
 var validatePresenceOf = function(value) {
-    return value && value.length;
+   return value && value.length;
 };
 
 /**
- * Methods
- */
+* Methods
+*/
 UserSchema.methods = {
 };
 
 mongoose.model('User', UserSchema);
-
