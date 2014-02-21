0325f84574fd5f4e31e217b607cd6b72d1d6431b
arkadyp <arkady.pevzner@gmail.com>
2014-02-20 16:54:00 -0800

    Stores loanId on $scope in loan.js

--- public/js/controllers/loan.js
+++ public/js/controllers/loan.js
@@ -3,18 +3,17 @@ angular.module('app')
 ["$scope", '$location', 'LendRequest',
 function($scope, $location, LendRequest){
   $scope.loan = null;
-  
-  var loanId = $location.path().slice($location.path().lastIndexOf('/')+1);
+  $scope.loanId = $location.path().slice($location.path().lastIndexOf('/')+1);
 
   //load loan from db
-  LendRequest.getLoan(loanId)
+  LendRequest.getLoan($scope.loanId)
   //success
   .then(function(loan){
     $scope.loan = loan;
   },
   //error 
   function(error){
-    console.log(error);
+    console.log('Error loading loan: '+error);
   });
 
 }]);
