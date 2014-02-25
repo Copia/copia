c9654d1c021b3ea487e7dd04c93c0cf46c478460
arkadyp <arkady.pevzner@gmail.com>
2014-02-24 17:51:57 -0800

    Adds venmo service to dashboard controller; includes venmo service script in index.html

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -1,12 +1,13 @@
 angular.module('app')
-.controller('DashboardController', ["$scope", '$cookieStore',
-function($scope, $cookieStore){
+.controller('DashboardController', ["$scope", '$cookieStore', 'VenmoAuthentication',
+function($scope, $cookieStore, VenmoAuthentication){
 
   $scope.venmoConnected = false;
-  $scope.token = $cookieStore.get('session_token');
+  $scope.session_token = $cookieStore.get('session_token');
+  $scope.user_id = $cookieStore.get('user_id');
 
   $scope.venmoAuth = function(){
-    
+    VenmoAuthentication.authenticate($scope.user_id, $scope.session_token);
   };
 
 }]);
