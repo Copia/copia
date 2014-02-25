0808f401d338363c00244745648e68f74451ea68
arkadyp <arkady.pevzner@gmail.com>
2014-02-24 19:09:48 -0800

    Fixes typo on dashboard controller

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -5,7 +5,7 @@ function($scope, CookieService, VenmoAuthentication){
   $scope.venmoConnected = false;
   var cookies = CookieService.getCookies();
   $scope.session_token = cookies.session_token;
-  $scope.user_id = cookies.used_id;
+  $scope.user_id = cookies.user_id;
 
   $scope.venmoAuth = function(){
     VenmoAuthentication.authenticate($scope.user_id, $scope.session_token);
