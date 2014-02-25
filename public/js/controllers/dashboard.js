d3d4ad73c70c323703d11dbbdd52796f3bdf06e5
arkadyp <arkady.pevzner@gmail.com>
2014-02-24 16:45:25 -0800

    Adds button to allow logged in user to connect to Venmo

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -2,6 +2,11 @@ angular.module('app')
 .controller('DashboardController', ["$scope", '$cookieStore',
 function($scope, $cookieStore){
 
+  $scope.venmoConnected = false;
   $scope.token = $cookieStore.get('session_token');
+
+  $scope.venmoAuth = function(){
+    
+  };
 
 }]);
