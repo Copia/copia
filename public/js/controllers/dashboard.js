c1e81ea9c25934a3a7380411d4863a37d5474d14
Will Gester <willgester@gmail.com>
2014-02-26 12:30:05 -0800

    corrected hidden buttons on dashboard

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -5,6 +5,8 @@ function($scope, CookieService, VenmoAuthentication, $location, ModifyUser, $roo
   var cookies = CookieService.getCookies();
   $scope.session_token = cookies.session_token;
   $scope.user_id = cookies.user_id;
+  $rootScope.venmoConnected = true;
+  $rootScope.showDashboard = false;
 
 
   ModifyUser.getUser(cookies).then();
