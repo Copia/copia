f2d284f0677346f3d14130775170a1811fd18467
Will Gester <willgester@gmail.com>
2014-02-25 17:09:39 -0800

    added logout controller

---
+++ public/js/controllers/logout.js
@@ -0,0 +1,6 @@
+angular.module('app')
+.controller('DashboardController', ["$scope", 'CookieService', 'VenmoAuthentication', '$location', 'CheckUser', '$rootScope',
+function($scope, CookieService, VenmoAuthentication, $location, CheckUser, $rootScope){
+  
+}]);
+
