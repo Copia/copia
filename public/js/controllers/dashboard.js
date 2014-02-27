195e0a0be7e70ad1db31c8b80ff1345fa439e9f5
Will Gester <willgester@gmail.com>
2014-02-26 16:09:29 -0800

    created repay loan, toggled loan buttons, created window method for toggle

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -1,6 +1,6 @@
 angular.module('app')
-.controller('DashboardController', ["$scope", 'CookieService', 'VenmoAuthentication', '$location', 'ModifyUser', '$rootScope', 'LendRequest',
-function($scope, CookieService, VenmoAuthentication, $location, ModifyUser, $rootScope, LendRequest){
+.controller('DashboardController', ["$scope", 'CookieService', 'VenmoAuthentication', '$location', 'ModifyUser', '$rootScope', 'LendRequest', '$timeout',
+function($scope, CookieService, VenmoAuthentication, $location, ModifyUser, $rootScope, LendRequest, $timeout){
   $rootScope.navbar = true;
   var cookies = CookieService.getCookies();
   $scope.session_token = cookies.session_token;
@@ -25,6 +25,10 @@ function($scope, CookieService, VenmoAuthentication, $location, ModifyUser, $roo
   $scope.repayLoan = function(){
     LendRequest.repayLoan('123', $scope.session_token, $scope.user_id);
   };
+
+  window.showmethemoney = function(){
+    $timeout(function(){$rootScope.showDashboard = false;},0);
+  }
 
 }]);
 
