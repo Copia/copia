1571d29e01f8e6a38534ae644ba32bef10f61e9c
Will Gester <willgester@gmail.com>
2014-02-27 11:50:48 -0800

    collapses navbar upon logout

--- public/js/controllers/navbar.js
+++ public/js/controllers/navbar.js
@@ -2,6 +2,12 @@ angular.module('app')
 .controller('NavbarController',  
 function($scope, $location, CookieService, ModifyUser, $rootScope){
   
+
+  $scope.isCollapsed = true;
+  $scope.toggleNavbar = function(){
+    $scope.isCollapsed = !$scope.isCollapsed;
+  }
+
   if ($location.path() !== '/'){
     $rootScope.navbar = true;
   }
@@ -14,6 +20,7 @@ function($scope, $location, CookieService, ModifyUser, $rootScope){
     ModifyUser.logoutUser(CookieService.getCookies());
     CookieService.eatCookies();
     $rootScope.navbar = false;
+    $scope.isCollapsed = true;
     $location.path( "/" );
   };
 
