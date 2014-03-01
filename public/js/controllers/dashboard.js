8d5bb5f3a4bb556b5760ca7816df1bdcde73f76b
Will Gester <willgester@gmail.com>
2014-02-28 17:05:54 -0800

    repay loan sends correct loan

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -54,7 +54,7 @@ function($scope, CookieService, VenmoAuthentication, $location, ModifyUser, $roo
   };
 
   $scope.repayLoan = function(){
-    LendRequest.repayLoan($scope.loan[0]._id, $scope.session_token, $scope.user_id).then(function() {$timeout(function() {$route.reload();},0);});
+    LendRequest.repayLoan($scope.loanView._id, $scope.session_token, $scope.user_id).then(function() {$timeout(function() {$route.reload();},0);});
   };
 
 }]);
