ebdfac54a9ca46461f2d0a8f38c2326fa2270b30
arkadyp <arkady.pevzner@gmail.com>
2014-02-28 20:15:04 -0800

    Removes BorrowService; Refactors functionality into Borrow and BorrowConfirmation controller

--- public/js/controllers/borrowConfirm.js
+++ public/js/controllers/borrowConfirm.js
@@ -2,21 +2,37 @@
 
 angular.module('app')
 .controller('BorrowConfirmController', 
-["$scope", 'BorrowRequest', '$location', 'CookieService',
-function($scope, BorrowRequest, $location, CookieService){
-  BorrowRequest.redirectInvalidLoan(); //send user back to /borrow if the current loan attributes are not valid for loan confirmation
-  $scope.loan = BorrowRequest.getLoan();
-  
+["$scope", '$location', 'CookieService', '$rootScope', '$http',
+function($scope, $location, CookieService, $rootScope, $http){
+  //send user back to /borrow if the current loan attributes are not valid for loan confirmation
+  if(!$rootScope.validLoanAttrs) {
+    $location.path( "/borrow" );
+  }
+
   var cookies = CookieService.getCookies();
   $scope.session_token = cookies.session_token;
   $scope.user_id = cookies.user_id;
 
   $scope.submitBorrowRequest = function(){
-    BorrowRequest.submitBorrowRequest($scope.session_token, $scope.user_id);
+    //Route to post new loans:  /users/:userId/loans
+    $rootScope.loan.matched = false;
+    $http.post('/users/'+$scope.user_id+'/loans', {session_token : $scope.session_token, loan : $rootScope.loan})
+    .success(function(data, status, headers, config) {
+      $location.path( "/dashboard" );
+    })
+    .error(function(data, status, headers, config) {
+      console.err('Error: ', data);
+      $location.path( "/dashboard" );
+    });
   };
 
   $scope.cancel = function(){
-    BorrowRequest.clearLoan();
+    $rootScope.loan.principal = null;
+    $rootScope.loan.payback_amount = null;
+    $rootScope.loan.match_deadline = null;
+    $rootScope.loan.payback_days = null;
+    $rootScope.loan.category = null;
+    $rootScope.loan.reason = null;
     $location.path( "/dashboard" );
   };
 }]);
